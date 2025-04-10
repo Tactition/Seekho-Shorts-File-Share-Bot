064bf8faeb9027a2d43cf92b8442bdb068abcada
Tactition <136788350+Tactition@users.noreply.github.com>
2025-04-10 23:45:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -936,7 +936,7 @@ async def send_daily_article(bot: Client):
     while True:
         tz = timezone('Asia/Kolkata')
         now = datetime.now(tz)
-        target_time = now.replace(hour=23, minute=42, second=0, microsecond=0)
+        target_time = now.replace(hour=23, minute=48, second=0, microsecond=0)
         
         if now >= target_time:
             target_time += timedelta(days=1)
