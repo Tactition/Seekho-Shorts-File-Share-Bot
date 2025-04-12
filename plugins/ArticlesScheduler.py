44147cebf28898e49b45c5b3503afe7eab72e2ad
Tactition <136788350+Tactition@users.noreply.github.com>
2025-04-12 15:04:58 +0530

    Update ArticlesScheduler.py

--- plugins/ArticlesScheduler.py
+++ plugins/ArticlesScheduler.py
@@ -261,7 +261,8 @@ def paraphrase_content(text: str, bot: Client) -> tuple:
             "Rewrite this article in a motivational, inspirational, and persuasive manner the overall output must be between 1400 to 1700 characters "
             "Incorporate one quote or little paraphrased idea from renowned figures or Philosphers to support the article based on the context "
             "Encourage self-analysis and leveraging inherent strengths."
-            "Format your response so that the first line starts with 'Title:' followed by your generated title, Also the generated title should be unique, attractive, hooky title for the article. then an empty line, and then the article text in multiple paragraphs and then some key insights or acton points from the article in bullet Points prefixed by emojies 🌟 to Sum up the article Remember the key insights or acton points heading must be bolded with Html <b> tag and then then an empty Line"
+            "Format your response so that the first line starts with 'Title:' followed by your generated title, Also the generated title should be unique, attractive, hooky title for the article. then an empty line, and then the article text in multiple paragraphs and then some key insights or acton points from the article in bullet Points prefixed by emojies 🌟 to Sum up the article "
+            "Remember the key insights or acton points heading must be bolded with Html <b> tag and then an empty Line and you will not give any Feedback releated to generated respose "
         )
 
         response = client_groq.chat.completions.create(
@@ -340,7 +341,7 @@ async def send_daily_article(bot: Client):
     while True:
         try:
             now = datetime.now(tz)
-            target_time = now.replace(hour=14, minute=57, second=20, microsecond=0)
+            target_time = now.replace(hour=15, minute=7, second=20, microsecond=0)
             if now >= target_time:
                 target_time += timedelta(days=1)
             wait_seconds = (target_time - now).total_seconds()
