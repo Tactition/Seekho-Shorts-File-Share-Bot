1dd41364bde237910e35d05cbf5c41328e0b69c9
Tactition <136788350+Tactition@users.noreply.github.com>
2025-04-12 14:25:48 +0530

    Update ArticlesScheduler.py

--- plugins/ArticlesScheduler.py
+++ plugins/ArticlesScheduler.py
@@ -341,7 +341,7 @@ async def send_daily_article(bot: Client):
     while True:
         try:
             now = datetime.now(tz)
-            target_time = now.replace(hour=14, minute=14, second=20, microsecond=0)
+            target_time = now.replace(hour=14, minute=28, second=20, microsecond=0)
             if now >= target_time:
                 target_time += timedelta(days=1)
             wait_seconds = (target_time - now).total_seconds()
