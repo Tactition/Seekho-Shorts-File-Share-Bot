ca59e33fe52a4dd3ad955e668bd9a2bf15bb6767
Tactition <136788350+Tactition@users.noreply.github.com>
2025-04-12 12:02:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ from plugins.clone import restart_bots
 from Zahid.bot import StreamBot
 from Zahid.utils.keepalive import ping_server  # Your ping script imported here
 from Zahid.bot.clients import initialize_clients
-from plugins.commands import schedule_daily_quotes, schedule_daily_articles
+from plugins.ArticlesScheduler import schedule_daily_quotes, schedule_daily_articles
 
 
 
